b6ed724ad36b15db6ba52b1fe91a796e8ff806f0
JEdwards <jordanscottedwards@gmail.com>
2023-03-02 21:15:27 -0500

    Built basic feedback page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Heading text='give feedback' />
+
+      <Button text='good' onClick={handleGoodClick} />
+      <Button text='neutral' onClick={handleNeutralClick} />
+      <Button text='bad' onClick={handleBadClick} />
+
+      <Heading text='statistics' />
+      <Stat text='good' count={good} />
+      <Stat text='neutral' count={neutral} />
+      <Stat text='bad' count={bad} />
+    </>
   );
+}
+
+const Heading = (props) => {
+  return (
+    <>
+      <h1>{props.text}</h1>
+    </>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.onClick}>{props.text}</button>
+    </>
+  )
+}
+
+const Stat = (props) => {
+  return (
+    <>
+      <p>{props.text} {props.count}</p>
+    </>
+  )
 }
 
 export default App;
